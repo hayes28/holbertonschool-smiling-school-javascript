2063c7c6f5d2685fdbc2ee390a60bdd24dea6916
HH <heather.hayes918@gmail.com>
2023-08-02 22:54:06 -0500

    home, price done. Not course

--- scripts.js
+++ scripts.js
@@ -219,3 +219,124 @@ $(function () {
 		});
 	});
 });
+
+// COURSES SEARCH/RESULTS SECTIONS
+function createCardCourse(course) {
+	const { thumb_url, title, 'sub-title': sub_title, author_pic_url, author, duration, star } = course;
+
+	let stars = '';
+	for (let i = 1; i <= 5; i++) {
+		if (i <= star) {
+			stars += '<img src="images/star_on.png" alt="star on" width="15px" height="15px">';
+		} else {
+			stars += '<img src="images/star_off.png" alt="star off" width="15px" height="15px">';
+		}
+	}
+
+	const cardContainer = $('<div>').addClass('col-12 col-sm-6 col-md-3 card-deck');
+	const card = $('<div>').addClass('card border-0 d-flex flex-column').appendTo(cardContainer);
+
+	const thumbnailContainer = $('<div>').addClass('thumbnail-container position-relative').appendTo(card);
+	$('<img>').attr({ 'src': thumb_url, 'alt': 'Video thumbnail' }).addClass('card-img-top').appendTo(thumbnailContainer);
+
+	const overlay = $('<div>').addClass('card-img-overlay text-center').appendTo(thumbnailContainer);
+	$('<img>').attr({ 'src': 'images/play.png', 'alt': 'Play Button', 'width': '64px' }).addClass('align-self-center play-overlay').appendTo(overlay);
+
+	const cardBody = $('<div>').addClass('card-body px-2').appendTo(card);
+	$('<h5>').addClass('card-title font-weight-bold').text(title).appendTo(cardBody);
+
+	$('<p>').addClass('card-text text-muted').text(sub_title).appendTo(cardBody);
+
+	const creator = $('<div>').addClass('creator d-flex align-items-center').appendTo(cardBody);
+	$('<img>').attr({ 'src': author_pic_url, 'alt': 'Author' }).css('width', '30px').addClass('rounded-circle').appendTo(creator);
+	$('<h6>').addClass('pl-3 m-0 main-color').text(author).appendTo(creator);
+
+	const info = $('<div>').addClass('info pt-3 d-flex justify-content-between').appendTo(cardBody);
+	const rating = $('<div>').addClass('rating d-flex').appendTo(info);
+	rating.append(stars);
+	$('<span>').addClass('main-color').text(duration).appendTo(info);
+
+	return cardContainer;
+}
+
+function updateSelections(data) {
+	// Set the first topic and sort option as selected
+	if (data.topics.length > 0) {
+		let topic = data.topics[0].replace(/_/g, ' ');
+		$('.box2 .btn span').text(topic);
+	}
+
+	if (data.sorts.length > 0) {
+		let sort = data.sorts[0].replace(/_/g, ' ');
+		$('.box3 .btn span').text(sort);
+	}
+}
+
+function populateCourses(coursesSelector, url, query = '', topic = 'all', sort = 'most_popular') {
+	const loader = $('.loader');
+	loader.show();
+
+	$.get(url, { q: query, topic: topic, sort: sort }, function (data) {
+		loader.hide();
+		$(coursesSelector).empty();
+
+		console.log(`Data fetched for ${coursesSelector}: `, data);
+
+		// Set the search value
+		$('#keywords').val(data.q);
+
+		// Update the topics, sorts dropdowns, and removes underscores
+
+		$('.box2 .dropdown-menu').html('');
+		data.topics.forEach(topic => {
+			topic = topic.replace(/_/g, ' ');
+			$('.box2 .dropdown-menu').append(`<a class="dropdown-item" href="#">${topic}</a>`);
+		});
+
+
+		$('.box3 .dropdown-menu').html('');
+		data.sorts.forEach(sort => {
+			sort = sort.replace(/_/g, ' ');
+			$('.box3 .dropdown-menu').append(`<a class="dropdown-item" href="#">${sort}</a>`);
+		});
+
+
+
+		// Update the selected options
+		updateSelections(data);
+
+		$('#section-title').html(`<span class="text-muted video-count">${data.courses.length} videos</span>`);
+
+		data.courses.forEach((course, index) => {
+			const card = createCardCourse(course);
+			$(coursesSelector).append(card);
+		});
+
+		// Reload courses when a new Topic is selected
+		$('.box2 .dropdown-item').on('click', function (e) {
+			e.preventDefault();
+			$('.box2 .btn span').text($(this).text());
+			populateCourses('#courses-list', coursesApiUrl, $('.search-text-area').val(), $(this).text(), $('.box3 .btn span').text());
+		});
+
+		// Reload courses when a new Sort by is selected
+		$('.box3 .dropdown-item').on('click', function (e) {
+			e.preventDefault();
+			$('.box3 .btn span').text($(this).text());
+			populateCourses('#courses-list', coursesApiUrl, $('.search-text-area').val(), $('.box2 .btn span').text(), $(this).text());
+		});
+	});
+}
+
+$(function () {
+	console.log("Document is ready");
+
+	const coursesApiUrl = "https://smileschool-api.hbtn.info/courses";
+
+	populateCourses('#courses-list', coursesApiUrl);
+
+	// Reload courses when search value changes
+	$('.search-text-area').on('change', function () {
+		populateCourses('#courses-list', coursesApiUrl, $(this).val(), $('.box2 .btn span').text(), $('.box3 .btn span').text());
+	});
+});
